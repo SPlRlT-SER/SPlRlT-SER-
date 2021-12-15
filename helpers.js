ef429138bd3ec3c7ee264f9e9b4b357b53858a19
SPlRlT <95764723+SPlRlT-SER@users.noreply.github.com>
2021-12-15 08:45:56 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 /*
-# Copyright (C) 2020 Princrudh.
+# Copyright (C) 2020 splrlt-ser.
 #
 # WhatsAsena is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
